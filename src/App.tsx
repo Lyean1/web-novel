afab3c05ffcec08f73ddd3994b256dad4fb1d8a4
Lyean <lyean1500@gmail.com>
2023-07-27 15:59:39 +0630

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 const App: React.FC = () => {
   return (
     <div className="app">
-      <Navbar title="KyunKone Kindergarten" />
+      <Navbar title="web " />
     </div>
   );
 };
